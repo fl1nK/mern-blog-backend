3bd5b0e8d4434f84b9e4bd655f2375255a3ce1b0
Vlad Filonenko <filonenkovlad5@gmail.com>
2024-08-04 22:20:32 +0300

    add cloudinary

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import mongoose from 'mongoose';
 import multer from 'multer';
 import cors from 'cors';
+import { v2 as cloudinary } from 'cloudinary';
 import 'dotenv/config';
 
 import authRouter from './routes/authRouter.js';
@@ -12,13 +13,13 @@ import { checkAuth } from './middleware/authMiddleware.js';
 
 const app = express();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const storage = multer.diskStorage({
-  destination: (_, __, cb) => {
-    if (!fs.existsSync('uploads')) {
-      fs.mkdirSync('uploads');
-    }
-    cb(null, 'uploads');
-  },
   filename: (_, file, cb) => {
     cb(null, Date.now() + file.originalname);
   },
@@ -29,12 +30,19 @@ const upload = multer({ storage });
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/uploads', express.static('uploads'));
 
-app.post('/upload', upload.single('image'), (req, res) => {
-  res.json({
-    url: `/uploads/${req.file.filename}`,
-  });
+app.post('/upload', upload.single('image'), async (req, res) => {
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path);
+    console.log(result.secure_url);
+
+    res.json({
+      url: result.secure_url,
+    });
+  } catch (error) {
+    console.error('Error uploading to Cloudinary:', error);
+    res.status(500).json({ error: 'Failed to upload image' });
+  }
 });
 
 app.use('/', authRouter);
@@ -45,7 +53,7 @@ mongoose
   .connect(process.env.DB_URL)
   .then(() => {
     app.listen(process.env.PORT || 5000);
-    console.log('DB connected and server us running.');
+    console.log('DB connected and server is running.');
   })
   .catch((err) => {
     console.log(err);
